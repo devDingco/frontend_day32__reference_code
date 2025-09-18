91f47ed3533d9feb57994b46f6a38c756855875b
최홍식 <hongsik@letsdingco.com>
2025-09-18 10:18:53 +0900

    refactor: import 문 변환

--- src/commons/layout/banner/index.tsx
+++ src/commons/layout/banner/index.tsx
@@ -5,69 +5,72 @@ import "swiper/css";
 import Image from "next/image";
 import { Pagination } from "swiper/modules";
 import "swiper/css/pagination";
+import banner1 from "@assets/banner1.png";
+import banner2 from "@assets/banner2.png";
+import banner3 from "@assets/banner3.png";
 const IMAGE_SRC = {
-  banner1: {
-    src: require("@assets/banner1.png"),
-    alt: "배너버튼1",
-  },
+	banner1: {
+		src: banner1,
+		alt: "배너버튼1",
+	},
 
-  banner2: {
-    src: require("@assets/banner2.png"),
-    alt: "배너버튼2",
-  },
+	banner2: {
+		src: banner2,
+		alt: "배너버튼2",
+	},
 
-  banner3: {
-    src: require("@assets/banner3.png"),
-    alt: "배너버튼3",
-  },
+	banner3: {
+		src: banner3,
+		alt: "배너버튼3",
+	},
 };
 
 export default function BannerList() {
-  return (
-    <Swiper
-      modules={[Pagination]}
-      slidesPerView={1}
-      allowSlideNext
-      allowSlidePrev
-      pagination={{
-        clickable: true,
-        renderBullet: (index, className) => {
-          return (
-            '<span class="' +
-            className +
-            " " +
-            styles.customBullet +
-            '"></span>'
-          );
-        },
-      }}
-    >
-      <SwiperSlide>
-        <Image
-          src={IMAGE_SRC.banner1.src}
-          alt={IMAGE_SRC.banner1.alt}
-          sizes="100vw"
-          style={{ width: "100%", height: "400px", objectFit: "cover" }}
-        />
-      </SwiperSlide>
+	return (
+		<Swiper
+			modules={[Pagination]}
+			slidesPerView={1}
+			allowSlideNext
+			allowSlidePrev
+			pagination={{
+				clickable: true,
+				renderBullet: (index, className) => {
+					return (
+						'<span class="' +
+						className +
+						" " +
+						styles.customBullet +
+						'"></span>'
+					);
+				},
+			}}
+		>
+			<SwiperSlide>
+				<Image
+					src={IMAGE_SRC.banner1.src}
+					alt={IMAGE_SRC.banner1.alt}
+					sizes="100vw"
+					style={{ width: "100%", height: "400px", objectFit: "cover" }}
+				/>
+			</SwiperSlide>
 
-      <SwiperSlide>
-        <Image
-          src={IMAGE_SRC.banner2.src}
-          alt={IMAGE_SRC.banner2.alt}
-          sizes="100vw"
-          style={{ width: "100%", height: "400px", objectFit: "cover" }}
-        />
-      </SwiperSlide>
+			<SwiperSlide>
+				<Image
+					src={IMAGE_SRC.banner2.src}
+					alt={IMAGE_SRC.banner2.alt}
+					sizes="100vw"
+					style={{ width: "100%", height: "400px", objectFit: "cover" }}
+				/>
+			</SwiperSlide>
 
-      <SwiperSlide>
-        <Image
-          src={IMAGE_SRC.banner3.src}
-          alt={IMAGE_SRC.banner3.alt}
-          sizes="100vw"
-          style={{ width: "100%", height: "400px", objectFit: "cover" }}
-        />
-      </SwiperSlide>
-    </Swiper>
-  );
+			<SwiperSlide>
+				<Image
+					src={IMAGE_SRC.banner3.src}
+					alt={IMAGE_SRC.banner3.alt}
+					sizes="100vw"
+					style={{ width: "100%", height: "400px", objectFit: "cover" }}
+				/>
+			</SwiperSlide>
+		</Swiper>
+	);
 }
--- src/components/boards-detail/comment-list-item/index.tsx
+++ src/components/boards-detail/comment-list-item/index.tsx
@@ -4,68 +4,72 @@ import { Rate } from "antd";
 import { IcommentList } from "./types";
 import { useState } from "react";
 import CommentWrite from "../comment-write";
+import profileImage from "@assets/profile_image.png";
+import starsImage from "@assets/fivestars.png";
+import editImage from "@assets/edit.png";
+import closeImage from "@assets/close.png";
 const IMAGE_SRC = {
-  profileImage: {
-    src: require("@assets/profile_image.png"),
-    alt: "프로필이미지",
-  },
-  starsImage: {
-    src: require("@assets/fivestars.png"),
-    alt: "평점이미지",
-  },
-  editImage: {
-    src: require("@assets/edit.png"),
-    alt: "편집버튼",
-  },
-  closeImage: {
-    src: require("@assets/close.png"),
-    alt: "삭제버튼",
-  },
+	profileImage: {
+		src: profileImage,
+		alt: "프로필이미지",
+	},
+	starsImage: {
+		src: starsImage,
+		alt: "평점이미지",
+	},
+	editImage: {
+		src: editImage,
+		alt: "편집버튼",
+	},
+	closeImage: {
+		src: closeImage,
+		alt: "삭제버튼",
+	},
 };
 
 export default function CommentItem({ comment, index, length }: IcommentList) {
-  const [isEditing, setIsEditing] = useState<boolean>(false);
-  return (
-    <div key={comment?._id}>
-      {isEditing ? (
-        <CommentWrite
-          comment={comment}
-          isCommentEdit={isEditing}
-          setIsCommetEdit={setIsEditing}
-        />
-      ) : (
-        <div className={styles.listBody}>
-          <div className={styles.listTitle}>
-            <div className={styles.forwardTitle}>
-              <Image
-                src={IMAGE_SRC.profileImage.src}
-                alt={IMAGE_SRC.profileImage.alt}
-              />
-              <div className={styles.forwardTitleText}>{comment?.writer}</div>
-              <Rate defaultValue={comment?.rating} />
-            </div>
-            <div>
-              <div className={styles.backTitle}>
-                <button onClick={() => setIsEditing(true)}>
-                  <Image
-                    src={IMAGE_SRC.editImage.src}
-                    alt={IMAGE_SRC.editImage.alt}
-                  />
-                </button>
-                <Image
-                  src={IMAGE_SRC.closeImage.src}
-                  alt={IMAGE_SRC.closeImage.alt}
-                />
-              </div>
-            </div>
-          </div>
-          <div className={styles.commentText}>{comment?.contents}</div>
-          <div className={styles.commentDate}>
-            {comment?.createdAt?.split("T")[0].split("-").join(".")}
-          </div>
-        </div>
-      )}
-      {index + 1 !== length && <div className={styles.border}></div>}
-    </div>
-  );
+	const [isEditing, setIsEditing] = useState<boolean>(false);
+	return (
+		<div key={comment?._id}>
+			{isEditing ? (
+				<CommentWrite
+					comment={comment}
+					isCommentEdit={isEditing}
+					setIsCommetEdit={setIsEditing}
+				/>
+			) : (
+				<div className={styles.listBody}>
+					<div className={styles.listTitle}>
+						<div className={styles.forwardTitle}>
+							<Image
+								src={IMAGE_SRC.profileImage.src}
+								alt={IMAGE_SRC.profileImage.alt}
+							/>
+							<div className={styles.forwardTitleText}>{comment?.writer}</div>
+							<Rate defaultValue={comment?.rating} />
+						</div>
+						<div>
+							<div className={styles.backTitle}>
+								<button onClick={() => setIsEditing(true)}>
+									<Image
+										src={IMAGE_SRC.editImage.src}
+										alt={IMAGE_SRC.editImage.alt}
+									/>
+								</button>
+								<Image
+									src={IMAGE_SRC.closeImage.src}
+									alt={IMAGE_SRC.closeImage.alt}
+								/>
+							</div>
+						</div>
+					</div>
+					<div className={styles.commentText}>{comment?.contents}</div>
+					<div className={styles.commentDate}>
+						{comment?.createdAt?.split("T")[0].split("-").join(".")}
+					</div>
+				</div>
+			)}
+			{index + 1 !== length && <div className={styles.border}></div>}
+		</div>
+	);
 }
