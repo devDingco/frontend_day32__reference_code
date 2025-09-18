0324bc4325c4a42ae7feae7f6c89b721cb71712f
최홍식 <hongsik@letsdingco.com>
2025-09-18 10:19:30 +0900

    design: openapis 목록 디자인 수정

--- src/components/openapis-list/index.tsx
+++ src/components/openapis-list/index.tsx
@@ -5,35 +5,37 @@ import { useGetCatList } from "./hook";
 import Image from "next/image";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { Skeleton } from "antd";
+import styles from "./styles.module.css";
 
 export default function CatList() {
-  const { page, setPage, onClickSync, cats } = useGetCatList();
+	const { page, setPage, onClickSync, cats } = useGetCatList();
 
-  useEffect(() => {
-    onClickSync();
-  }, [page]);
+	useEffect(() => {
+		onClickSync();
+	}, [page]);
 
-  return (
-    <div>
-      <InfiniteScroll
-        hasMore={true}
-        loader={<Skeleton />}
-        dataLength={cats.length ?? 0}
-        next={() => setPage((prev) => prev + 1)}
-      >
-        {cats?.map((cat, index) => {
-          return (
-            <div key={cat.id}>
-              <Image
-                src={cat.url}
-                alt={"고양이이미지" + index}
-                width={cat.width}
-                height={cat.height}
-              />
-            </div>
-          );
-        })}
-      </InfiniteScroll>
-    </div>
-  );
+	return (
+		<div className={styles.catList}>
+			<h1>고양이 이미지 목록</h1>
+			<InfiniteScroll
+				hasMore={true}
+				loader={<Skeleton />}
+				dataLength={cats.length ?? 0}
+				next={() => setPage((prev) => prev + 1)}
+			>
+				{cats?.map((cat, index) => {
+					return (
+						<div key={cat.id} className={styles.catImage}>
+							<Image
+								src={cat.url}
+								alt={"고양이이미지" + index}
+								width={cat.width}
+								height={cat.height}
+							/>
+						</div>
+					);
+				})}
+			</InfiniteScroll>
+		</div>
+	);
 }
